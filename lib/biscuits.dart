3af12b725508d4c4318945e684881749ca0684ff
Cédric Belin <cedric@belin.io>
2017-09-17 15:00:22 +0200

    Implemented the `CookieOptions` class

--- lib/biscuits.dart
+++ lib/biscuits.dart
@@ -1,6 +1,11 @@
 /// Service for interacting with the [HTTP cookies](https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies).
 library biscuits;
 
-part 'src/change_event.dart';
+import 'dart:async';
+import 'dart:collection';
+import 'dart:convert';
+//import 'dart:html' as dom;
+
 part 'src/cookie_options.dart';
 part 'src/cookies.dart';
+part 'src/key_value_change.dart';
--- lib/src/cookie_options.dart
+++ lib/src/cookie_options.dart
@@ -2,4 +2,38 @@ part of biscuits;
 
 /// Defines the attributes of a HTTP cookie.
 class CookieOptions {
+
+  /// Creates new cookie options.
+  CookieOptions({this.domain = '', this.expires, this.path = '', this.secure = false});
+
+  /// The domain for which the cookie is valid.
+  final String domain;
+
+  /// The expiration date and time for the cookie.
+  final DateTime expires;
+
+  /// The path to which the cookie applies.
+  final String path;
+
+  /// Value indicating whether to transmit the cookie over HTTPS only.
+  final bool secure;
+
+  /// Converts this object to a map in JSON format.
+  Map<String, dynamic> toJson() => {
+    'domain': domain,
+    'expires': expires != null ? expires.toIso8601String() : null,
+    'path': path,
+    'secure': secure
+  };
+
+  /// Returns a string representation of this object.
+  @override
+  String toString() {
+    var value = [];
+    if (expires != null) value.add('expires=${expires.toIso8601String()}');
+    if (domain.isNotEmpty) value.add('domain=$domain');
+    if (path.isNotEmpty) value.add('path=$path');
+    if (secure) value.add('secure');
+    return value.join('; ');
+  }
 }
