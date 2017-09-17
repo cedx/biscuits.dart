44a637aa8585b41f98acef99208ab25a13f1599a
Cédric Belin <cedric@belin.io>
2017-09-17 15:12:41 +0200

    Added the skeleton of the `CookieOptions` unit tests

---
+++ test/cookie_options_test.dart
@@ -0,0 +1,29 @@
+import 'package:biscuits/biscuits.dart';
+import 'package:test/test.dart';
+
+/// Tests the features of the [CookieOptions] class.
+void main() => group('CookieOptions', () {
+  group('.toJson()', () {
+    test('should return a map with default values for a newly created instance', () {
+      var map = new CookieOptions().toJson();
+      expect(map, hasLength(4));
+    });
+
+    test('should return a non-empty map for an initialized instance', () {
+      var map = new CookieOptions().toJson();
+      expect(map, hasLength(4));
+    });
+  });
+
+  group('.toString()', () {
+    var data = new CookieOptions().toString();
+
+    test('should start with the class name', () {
+      expect(data, contains('CookieOptions {'));
+    });
+
+    test('should contain the instance properties', () {
+      // TODO
+    });
+  });
+});
