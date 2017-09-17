83caacc2f0268da14821925131a7f86b704f929d
Cédric Belin <cedric@belin.io>
2017-09-17 14:56:24 +0200

    Implemented the `KeyValueChange` class

--- lib/src/key_value_change.dart
+++ lib/src/key_value_change.dart
@@ -1,5 +1,28 @@
 part of biscuits;
 
-/// Represents the event parameter used for a change event.
-class ChangeEvent {
+/// Represents the event parameter used for a cookie change event.
+class KeyValueChange {
+
+  /// Creates a new key-value change record.
+  KeyValueChange(this.key, {this.currentValue, this.previousValue});
+
+  /// The current value for the cookie, or a `null` reference if removed.
+  final String currentValue;
+
+  /// The cookie name.
+  final String key;
+
+  /// The previous value for the cookie, or a `null` reference if added.
+  final String previousValue;
+
+  /// Converts this object to a map in JSON format.
+  Map<String, dynamic> toJson() => {
+    'key': key,
+    'currentValue': currentValue,
+    'previousValue': previousValue
+  };
+
+  /// Returns a string representation of this object.
+  @override
+  String toString() => '$runtimeType ${JSON.encode(this)}';
 }
