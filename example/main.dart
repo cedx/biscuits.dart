4a22e5df42d54a26cee2b15d1c8434e259b70b36
Cédric Belin <cedric@belin.io>
2018-07-08 11:22:35 +0200

    Updated the example

--- example/main.dart
+++ example/main.dart
@@ -1,15 +1,41 @@
 import 'package:biscuits/biscuits.dart';
 
-/// TODO
-void main() {
-  var cookies = Cookies();
+/// Tests the cookie service.
+void writeCookies() {
+  final _cookies = Cookies();
 
-  cookies['foo'] = 'bar';
-  print(cookies['foo'].runtimeType); // "String"
-  print(cookies['foo']); // "bar"
+  // Write the cookies.
+  print(_cookies.containsKey('foo')); // false
+  print(_cookies.containsKey('baz')); // false
+  print(_cookies.length); // 0
 
-  cookies.setObject('bar', <String, int>{'baz': 123});
-  print(cookies.getObject('bar').runtimeType); // "_JsonMap"
-  print(cookies.getObject('bar')); // {"baz": 123}
-  print(cookies.getObject('bar')['baz']); // 123
+  _cookies['foo'] = 'bar';
+  print(_cookies.containsKey('foo')); // true
+  print(_cookies.length); // 1
+
+  _cookies.setObject('baz', <String, int>{'qux': 123});
+  print(_cookies.containsKey('baz')); // true
+  print(_cookies.length); // 2
+
+  // Read the cookies.
+  print(_cookies['foo']); // null
+  print(_cookies['baz']); // null
+
+  _cookies['foo'] = 'bar';
+  print(_cookies['foo'].runtimeType); // "String"
+  print(_cookies['foo']); // "bar"
+
+  _cookies.setObject('baz', <String, int>{'qux': 123});
+  print(_cookies.getObject('baz').runtimeType); // "_JsonMap"
+  print(_cookies.getObject('baz')); // {"qux": 123}
+  print(_cookies.getObject('baz')['qux']); // 123
+
+  // Delete the cookies.
+  _cookies.remove('foo');
+  print(_cookies.containsKey('foo')); // false
+  print(_cookies.length); // 1
+
+  _cookies.clear();
+  print(_cookies.containsKey('baz')); // false
+  print(_cookies.length); // 0
 }
