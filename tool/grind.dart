494b62928d640ee75b2752e8e1a38b8c91751e2a
Cédric Belin <cedric@belin.io>
2018-11-19 23:02:23 +0100

    Updated the build system

--- tool/grind.dart
+++ tool/grind.dart
@@ -29,7 +29,7 @@ void fix() => DartFmt.format(existingSourceDirs, lineLength: 200);
 void lint() => Analyzer.analyze(existingSourceDirs);
 
 @Task('Runs the test suites')
-void test() => TestRunner().test();
+void test() => Pub.run('build_runner', arguments: ['test']);
 
 @Task('Upgrades the project to the latest revision')
 void upgrade() {
