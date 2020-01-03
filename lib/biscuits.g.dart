1c26bb63b22aceb9fba2535123719320e3f73ab4
Cédric Belin <cedric@belin.io>
2020-01-03 18:18:20 +0100

    Updated the generated files

--- lib/biscuits.g.dart
+++ lib/biscuits.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'biscuits.dart';
+part of biscuits;
 
 // **************************************************************************
 // JsonSerializableGenerator
